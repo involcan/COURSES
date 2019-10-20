6553617368a0a4e61d77f2ccb2d735acb3316f8c
Instituto Volcanológico de Canarias <ldauria@iter.es>
2019-10-20 21:19:55 +0100

    Add files via upload

--- INVERSE_METHODS/0.1_intro_numpy.py
+++ INVERSE_METHODS/0.1_intro_numpy.py
@@ -1,29 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
-# Create list
+# Create a list
 l=[1,2,3,4]
 print(l)
-
-
-# In[ ]:
 
 
 l=[[1,'c'],[3,4.5]]
 print(l)
 
 
-# In[ ]:
-
-
 l=[1,2,3,4]
 l=l+1
-
-
-# In[ ]:
 
 
 import numpy
@@ -33,14 +21,8 @@ a=numpy.array([1,2,3,4])
 print(a)
 
 
-# In[ ]:
-
-
 a=a+1
 print(a)
-
-
-# In[ ]:
 
 
 # Create a matrix
@@ -48,70 +30,40 @@ b=numpy.array([[1,2,3,4],[3,2,4,2],[4,8,7,1]])
 print(b)
 
 
-# In[ ]:
-
-
 # Shape of a matrix
 print(b.shape)
-
-
-# In[ ]:
 
 
 print(b.shape[0])
 
 
-# In[ ]:
-
-
 # Access the matrix element
-# first line, first colums
+# first line, first columns
 print(b[0,0])
-
-
-# In[ ]:
 
 
 # Second line, third column
 print(b[1][2])
 
 
-# In[ ]:
-
-
 # Second line, third column
 print(b[1,2])
-
-
-# In[ ]:
 
 
 # First line
 print(b[0])
 
 
-# In[ ]:
-
-
 # First line (alternative)
 print(b[0,:])
-
-
-# In[ ]:
 
 
 # Second column
 print(b[:,1])
 
 
-# In[ ]:
-
-
 # Submatrix
 print(b[0:2,0:2])
-
-
-# In[ ]:
 
 
 # Submatrix
@@ -125,29 +77,17 @@ print(b[::2,::2])
 print(b.T)
 
 
-# In[ ]:
-
-
 # Square matrix
 b=b[0:3,0:3]
 print(b)
-
-
-# In[ ]:
 
 
 # Symmetric matrix
 print(b+b.T)
 
 
-# In[ ]:
-
-
 # Antisymmetric matrix
 print(b-b.T)
-
-
-# In[ ]:
 
 
 # Inverse matrix
@@ -155,49 +95,28 @@ ib=numpy.linalg.inv(b)
 print(ib)
 
 
-# In[ ]:
-
-
 # Element by element
 print(b*ib)
-
-
-# In[ ]:
 
 
 # Matrix product
 print(numpy.dot(b,ib))
 
 
-# In[ ]:
-
-
 # Determinant
 print(numpy.linalg.det(b))
-
-
-# In[ ]:
 
 
 # Identity matrices
 print(numpy.eye(3))
 
 
-# In[ ]:
-
-
 # Null matrices
 print(numpy.zeros((3,4)))
 
 
-# In[ ]:
-
-
 # Constant matrix
 print(numpy.full((2,3), fill_value=0.5))
-
-
-# In[ ]:
 
 
 # Eigenvalues and eigenvectors
@@ -209,14 +128,8 @@ print("Eigenvectors")
 print(evect)
 
 
-# In[ ]:
-
-
 # First eigenvector (First column of evect matrix)
 print(evect[:,0])
-
-
-# In[ ]:
 
 
 # Be careful when copying!! 
@@ -230,9 +143,6 @@ print("b")
 print(b)
 
 
-# In[ ]:
-
-
 # Be careful when copying!! 
 # ¡¡¡This is right!!!
 
@@ -244,9 +154,6 @@ print("c")
 print(c)
 print("b")
 print(b)
-
-
-# In[ ]:
 
 
 # Alternative
